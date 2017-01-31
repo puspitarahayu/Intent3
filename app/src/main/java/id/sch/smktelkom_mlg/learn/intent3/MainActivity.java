ce88013938f5e2e53ea89f858f04089a61b89718
Puspita <Rahayupuspita571@gmail.com>
2017-01-31 09:15:43 +0700

    Intent SMS

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import static android.R.id.message;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -20,6 +22,20 @@ public class MainActivity extends AppCompatActivity {
                         dialPhoneNumber("0341712500");
                     }
                 });
+        findViewById(R.id.imageViewSMS).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                composeSmsMessage("Pesan dari SMK Telkom Malang");
+            }
+        });
+    }
+
+    private void composeSmsMessage(String massage) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra("sms_body", message);
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
     }
 
     private void dialPhoneNumber(String phoneNumber) {
